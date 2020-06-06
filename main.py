16285d024f13f754e55e613fa970db1dfd67d0fe
soukhind2 <skndas@ucdavis.edu>
2020-06-06 11:30:15 -0700

    Added figures and plots.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ print('num TRs per run = %d' % (vdc_TRs_run))
 
 #%%
 sub_id = 2
-mask_name = '' # This is set in order to reduce memory demands in order to run within 4Gb, however, if you want to make this run on whole brain, then set this to ''
+mask_name = 'FFA' # This is set in order to reduce memory demands in order to run within 4Gb, however, if you want to make this run on whole brain, then set this to ''
 
 # Specify the subject name
 sub = 'sub-%.2d' % (sub_id)
@@ -106,7 +106,8 @@ plt.title("Correlation Matrix of 500 voxels",size = 20)
 #%%
 # We now use the PCA function in scikit-learn to reduce the dimensionality of the data
 # The number of components was chosen arbitrarily.
-pca = PCA(n_components=20)
+n = 40
+pca = PCA(n_components=n)
 bold_pca = pca.fit_transform(bold_normalized)
 
 print('Original data shape:', bold_normalized.shape)
@@ -119,15 +120,15 @@ n_bins=75
 n_plots = 4
 components_to_plot = [0,1,2,19]
 f, axes = plt.subplots(1, n_plots, figsize=(14, 14/n_plots))
-st=f.suptitle("Figure 3.1. Histogram of values for each PC dimension ", fontsize="x-large")
+#st=f.suptitle("Figure 3.1. Histogram of values for each PC dimension ", fontsize="x-large")
 
 for i in range(n_plots): 
     axes[i].hist(bold_pca[:, components_to_plot[i]], 
                  bins=n_bins)
     # mark the plots 
-    axes[i].set_title('PC Dimension %d'%(components_to_plot[i]+1))
-    axes[i].set_ylabel('Frequency')
-    axes[i].set_xlabel('Value')    
+    axes[i].set_title('PC Dimension %d'%(components_to_plot[i]+1),size = 20)
+    axes[i].set_ylabel('Frequency',size = 20)
+    axes[i].set_xlabel('Value',size = 20)    
     axes[i].set_xticks([])
     axes[i].set_yticks([])    
 
@@ -138,11 +139,11 @@ f.subplots_adjust(top=0.75)
 # Setting plotting parameters
 alpha_val = .8
 cur_pals = sns.color_palette('colorblind', n_colors=vdc_n_runs)
-
+ 
 # Plot
 n_plots = 3 
 f, axes = plt.subplots(1, n_plots, figsize=(14,5))
-st=f.suptitle("Figure 3.2. Scatter plots comparing PCA dimensions ", fontsize="x-large")
+#st=f.suptitle("Figure 3.2. Scatter plots comparing PCA dimensions ", fontsize="x-large")
 
 # plot data
 axes[0].scatter(bold_pca[:, 0], bold_pca[:, 1], 
@@ -152,9 +153,9 @@ axes[1].scatter(bold_pca[:, 2], bold_pca[:, 3],
 axes[2].scatter(bold_pca[:, 18], bold_pca[:, 19], 
                 alpha=alpha_val, marker='.', color = 'k')
 
-axes[0].set_title('PCA Dimensions\n1 x 2')
-axes[1].set_title('PCA Dimensions\n3 x 4')
-axes[2].set_title('PCA Dimensions\n18 x 19')
+axes[0].set_title('PCA Dimensions\n1 x 2',size = 20)
+axes[1].set_title('PCA Dimensions\n3 x 4',size = 20)
+axes[2].set_title('PCA Dimensions\n18 x 19',size = 20)
 
 # modifications that are common to all plots 
 for i in range(n_plots): 
@@ -162,22 +163,28 @@ for i in range(n_plots):
     axes[i].set_xticks([])
     axes[i].set_yticks([])
 
-f.tight_layout()
-st.set_y(0.95)
-f.subplots_adjust(top=0.75)
 #%% Scree Plot
 
-fig = plt.figure(figsize=(8,5))
-sing_vals = np.arange(20) + 1
-plt.plot(sing_vals, pca.explained_variance_ratio_, 'ro-', linewidth=2)
-plt.title('Scree Plot')
-plt.xlabel('Principal Component')
-plt.ylabel('Eigenvalue')
-plt.xticks(np.arange(1,21))
+fig,ax = plt.subplots()
 
-leg = plt.legend(['Eigenvalues from PCA'], loc='best', 
-                 shadow=False,markerscale=0.4)
+sing_vals = np.arange(n) + 1
+cumsum = np.cumsum(pca.explained_variance_ratio_)
+plt.plot(sing_vals, cumsum, 'ro-', linewidth=2)
+#plt.title('Scree Plot',size = 20)
+plt.xlabel('Principal Component',size = 20)
+plt.ylabel('Cumulative Variance',size = 20)
+plt.xticks(np.arange(1,n+1,2),size=15)
+#plt.ylim(0,1)
+plt.xlim(0,n+1)
+
+#leg = plt.legend(['Cumulative Explained Variance'], loc='best',shadow=False,
+               #  markerscale=0.4)
 leg.get_frame().set_alpha(0.4)
+
+#ax.vlines(x=19,ymax = 0.6, ymin = 0, color='grey')
+#ax.hlines(y=0.6,xmax = 19, xmin = 0, color='grey')
+ax.tick_params(axis='both', which='major', labelsize=20)
+ax.tick_params(axis='both', which='minor', labelsize=20)
 plt.show()
 
 #%%
@@ -194,10 +201,8 @@ end = time()
 print('Accuracy: ', scores)
 print('Run time: %0.4fs' %(end - start))
 
-#%%
-
-# Run the classifier on data in component space 
-pca = PCA(n_components=10)
+#%% Regular PCA
+pca = PCA(n_components= 19)
 bold_pca_normalized = pca.fit_transform(bold_normalized)
 print('PCA (c=%d) classification' % bold_pca_normalized.shape[1])
 print('New size after PCA: ', bold_pca_normalized.shape)
@@ -207,22 +212,68 @@ models_pca, scores_pca = decode(bold_pca_normalized, labels, run_ids, svc)
 end = time()
 print('Accuracy: ', scores_pca)
 print('Run time: %0.4fs' %(end - start))
+#%% PCA loop
 
-#%% Pipeline
+pcatime = np.zeros(45)
+# Run the classifier on data in component space 
+for i in range(1,45):
+    pca = PCA(n_components=i)
+    bold_pca_normalized = pca.fit_transform(bold_normalized)
+    #print('PCA (c=%d) classification' % bold_pca_normalized.shape[1])
+    #print('New size after PCA: ', bold_pca_normalized.shape)
+    
+    start = time()
+    models_pca, scores_pca = decode(bold_pca_normalized, labels, run_ids, svc)
+    end = time()
+    pcatime[i-1] = end - start
+    #sys.stdout.flush()
 
-# Set up the pipeline
-pipe = Pipeline([
-    ('reduce_dim', PCA(n_components=10)),
-    ('classify', SVC(kernel="linear", C=1)),
-])
-
-# Run the pipeline with cross-validation
+print('Accuracy: ', scores_pca)
+print('Run time: %0.4fs' %(end - start))
+#%% Baseline data cross validation
 ps = PredefinedSplit(run_ids) # Provides train/test indices to split data in train/test sets
-clf_pipe = cross_validate(
-    pipe,bold_normalized,labels,cv=ps,
-    return_train_score=True
-)
+clf_pipe = cross_validate(SVC(kernel="linear", C=1),bold_normalized,labels,cv=ps,
+        return_train_score=True)
+print(clf_pipe)
+print ("Average Testing Accuracy: %0.2f" % (np.mean(clf_pipe['test_score'])))
 
+#%% PCA cross validation
+pipe = Pipeline([
+                ('reduce_dim', PCA(n_components=19)),
+                ('classify', SVC(kernel="linear", C=1)),])
+
+ps = PredefinedSplit(run_ids) # Provides train/test indices to split data in train/test sets
+clf_pipe = cross_validate(pipe,bold_normalized,labels,cv=ps,
+                              return_train_score=True)
+
+print(clf_pipe)
+print ("Average Testing Accuracy: %0.2f" % (np.mean(clf_pipe['test_score'])))
+#%% Pipeline
+for ncomp in range(1,30):
+    # Set up the pipeline
+    pipe = Pipeline([
+        ('reduce_dim', PCA(n_components = ncomp)),
+        ('classify', SVC(kernel="linear", C=1)),
+    ])
+    
+    # Run the pipeline with cross-validation
+    ps = PredefinedSplit(run_ids) # Provides train/test indices to split data in train/test sets
+    clf_pipe = cross_validate(
+        pipe,bold_normalized,labels,cv=ps,
+        return_train_score=True
+    )
+    if ncomp == 1:
+        pca_score = clf_pipe['test_score']
+        fit_time_all = clf_pipe['fit_time']
+    else:
+        pca_score = np.vstack((pca_score,clf_pipe['test_score']))
+        fit_time_all = np.vstack((fit_time_all,clf_pipe['fit_time']))
+
+    print(ncomp)
+    
+all_score = np.mean(pca_score,1).reshape(pca_score.shape[0],1)
+all_time = np.cumsum(fit_time_all,1)
+pca_score = np.hstack((pca_score,all_score))
 # Print results from this dimensionality reduction technique
 print(clf_pipe)
 print ("Average Testing Accuracy: %0.2f" % (np.mean(clf_pipe['test_score'])))
---
+++ plots.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  1 07:48:42 2020
+
+@author: soukhind
+"""
+
+'''
+Works in conjunction with main.py ONLY
+Soukhin Das
+06th June 2020
+'''
+
+#%%
+logtime = np.log(pcatime)
+fig = plt.figure()
+
+plt.plot(logtime)
+plt.xlabel("log(time)")
+plt.ylabel
+plt.xlim(0,40)
+plt.xticks(np.arange(41))
+
+#%%
+# Plot accuracy vs pca components
+plt.style.use('seaborn-whitegrid')
+fig= plt.figure(figsize = (12,8))
+ax = fig.add_subplot(111)
+plt.plot(pca_score[:,0],'-D',label = 'Face',c = 'lime' , alpha = 0.4)
+plt.plot(pca_score[:,1],'-*',label = 'Scene',c = 'blue', alpha = 0.4)
+plt.plot(pca_score[:,2],'-x',label = 'Object',c = 'orange', alpha = 0.4)
+plt.plot(pca_score[:,3],'-o',label = 'Overall',c = 'red',linewidth = 2)   
+plt.title("Classifier Performance on Training Data",size = 25)
+l = plt.legend(loc = 4,title = 'Category')
+plt.setp(l.get_title(), multialignment='center')
+#ax.set_xlabel('Upper Bound of SOA')
+ax.set_ylabel('Classification Accuracy',size = 25)
+ax.set_xlabel('Principal Components',size = 25)
+ax.tick_params(axis='both', which='major', labelsize=20)
+ax.tick_params(axis='both', which='minor', labelsize=20)
+#%%
+#plot pca cross validation time
+fig = plt.figure()
+plt.style.use('seaborn-whitegrid')
+plt.plot(all_time[:,2])
+plt.xlabel("PCA Components",size = 20)
+plt.ylabel("Time",size = 20)
+plt.title("TIme taken for SVM with 3 fold Cross Validation",size = 20)
+ax.tick_params(axis='both', which='major', labelsize=20)
+ax.tick_params(axis='both', which='minor', labelsize=20)
